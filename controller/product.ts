5655c34b888b181c0a5b0e8ce5d3cdefcf014d1d
AshishKohad27 <kohadashish27@gmail.com>
2023-02-28 15:14:06 +0530

    controllers added

---
+++ controller/product.ts
@@ -0,0 +1,113 @@
+import productM from "../model/product";
+
+type ObjectProduct = {
+  _id?: String;
+  title: String;
+  image: String;
+  description: String;
+  price: Number;
+  quantity: Number;
+};
+
+const getProduct = async (): Promise<any> => {
+  //async function return promise that what typescript needed
+  try {
+    let data: Array<ObjectProduct> = await productM.find();
+    // console.log("data:", data);
+    return {
+      data,
+      dataLength: data.length || 0,
+      flag: true,
+      desc: "",
+      message: "Product Get Successfully!",
+    };
+  } catch (e: any) {
+    return {
+      data: [],
+      dataLength: 0,
+      flag: true,
+      desc: e.message,
+      message: "Error Occurs!",
+    };
+  }
+};
+
+const addProduct = async ({
+  title,
+  image,
+  description,
+  price,
+  quantity,
+}: ObjectProduct): Promise<any> => {
+  console.log("description:", description);
+  try {
+    let addProduct = new productM({
+      title,
+      image,
+      description,
+      price,
+      quantity,
+    });
+    await addProduct.save();
+
+    let data: Array<ObjectProduct> = await productM.find();
+    // console.log("data:", data);
+    return {
+      data,
+      dataLength: data.length || 0,
+      flag: true,
+      desc: "",
+      message: "Product Add Successfully!",
+    };
+  } catch (e: any) {
+    return {
+      data: [],
+      dataLength: 0,
+      flag: true,
+      desc: e.message,
+      message: "Error Occurs!",
+    };
+  }
+};
+
+const updateProduct = async ({
+  _id,
+  title,
+  image,
+  description,
+  price,
+  quantity,
+}: ObjectProduct): Promise<any> => {
+  try {
+    await productM.findByIdAndUpdate(
+      { _id },
+      {
+        title,
+        image,
+        description,
+        price,
+        quantity,
+      }
+    );
+
+    let data: Array<ObjectProduct> = await productM.find();
+    // console.log("data:", data);
+    return {
+      data,
+      dataLength: data.length || 0,
+      flag: true,
+      desc: "",
+      message: "Product Update Successfully!",
+    };
+  } catch (e: any) {
+    return {
+      data: [],
+      dataLength: 0,
+      flag: true,
+      desc: e.message,
+      message: "Error Occurs!",
+    };
+  }
+};
+
+export { getProduct, addProduct, updateProduct };
--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,6 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import productM from "../../../model/product";
 import connectDB from "../../../config/db";
+import {
+  getProduct,
+  addProduct,
+  updateProduct,
+} from "../../../controller/product";
 
 type ObjectProduct = {
   _id?: String;
@@ -24,143 +29,40 @@ export default async function productRoutes(
   res: NextApiResponse<TProduct>
 ) {
   await connectDB();
-
   console.log("Method:", req.method);
 
   if (req.method === "GET") {
     const { data, dataLength, flag, message, desc }: TProduct =
       await getProduct();
 
-    console.log("message:", message);
     if (flag) {
       return res.status(200).send({ message, desc, dataLength, data });
     } else {
       return res.status(401).send({ message, desc, dataLength, data });
     }
-  } else if (req.method === "POST") {
+  } 
+  else if (req.method === "POST") {
     const Body = req.body;
 
     const { data, dataLength, flag, message, desc }: TProduct =
       await addProduct(Body);
-    console.log("message:", message);
 
     if (flag) {
       return res.status(200).send({ message, desc, dataLength, data });
     } else {
       return res.status(401).send({ message, desc, dataLength, data });
     }
-  } else if (req.method === "PATCH") {
+  } 
+  else if (req.method === "PATCH") {
     const Body: ObjectProduct = req.body;
 
     const { data, dataLength, flag, message, desc }: TProduct =
       await updateProduct(Body);
-    console.log("message:", message);
 
     if (flag) {
       return res.status(200).send({ message, desc, dataLength, data });
     } else {
       return res.status(401).send({ message, desc, dataLength, data });
     }
-  }
-}
-
-async function getProduct(): Promise<any> {
-  //async function return promise that what typescript needed
-  try {
-    let data: Array<ObjectProduct> = await productM.find();
-    // console.log("data:", data);
-    return {
-      data,
-      dataLength: data.length || 0,
-      flag: true,
-      desc: "",
-      message: "Product Get Successfully!",
-    };
-  } catch (e: any) {
-    return {
-      data: [],
-      dataLength: 0,
-      flag: true,
-      desc: e.message,
-      message: "Error Occurs!",
-    };
-  }
-}
-
-async function addProduct({
-  title,
-  image,
-  description,
-  price,
-  quantity,
-}: ObjectProduct): Promise<any> {
-  console.log("description:", description);
-  try {
-    let addProduct = new productM({
-      title,
-      image,
-      description,
-      price,
-      quantity,
-    });
-    await addProduct.save();
-
-    let data: Array<ObjectProduct> = await productM.find();
-    // console.log("data:", data);
-    return {
-      data,
-      dataLength: data.length || 0,
-      flag: true,
-      desc: "",
-      message: "Product Add Successfully!",
-    };
-  } catch (e: any) {
-    return {
-      data: [],
-      dataLength: 0,
-      flag: true,
-      desc: e.message,
-      message: "Error Occurs!",
-    };
-  }
-}
-
-async function updateProduct({
-  _id,
-  title,
-  image,
-  description,
-  price,
-  quantity,
-}: ObjectProduct): Promise<any> {
-  try {
-    await productM.findByIdAndUpdate(
-      { _id },
-      {
-        title,
-        image,
-        description,
-        price,
-        quantity,
-      }
-    );
-
-    let data: Array<ObjectProduct> = await productM.find();
-    // console.log("data:", data);
-    return {
-      data,
-      dataLength: data.length || 0,
-      flag: true,
-      desc: "",
-      message: "Product Update Successfully!",
-    };
-  } catch (e: any) {
-    return {
-      data: [],
-      dataLength: 0,
-      flag: true,
-      desc: e.message,
-      message: "Error Occurs!",
-    };
   }
 }
