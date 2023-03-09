4ec4a14c7e5fc666447da20bd8029036b3b4f1d5
AshishKohad27 <kohadashish27@gmail.com>
2023-03-09 13:24:11 +0530

    ERROR RESOLVE

--- components/SingleProduct.tsx
+++ components/SingleProduct.tsx
@@ -17,7 +17,7 @@ export default function SingleProduct({ item }: any): any {
   // console.log("item:", item);
   return (
     <div className={style.container}>
-      <img src={item.image} alt={item.title} />
+      {/* <img src={item.image} alt={item.title} /> */}
       <h1>{item.title}</h1>
       <h1>Price: {item.bar}</h1>
       <button>Delete</button>
---
+++ pages/home/index.tsx
@@ -0,0 +1,10 @@
+import { Box, Container } from "@mui/system";
+import style from "../../styles/main.module.css";
+
+export default function Home() {
+  return (
+    <Container className={style.container}>
+      <div></div>
+    </Container>
+  );
+}
