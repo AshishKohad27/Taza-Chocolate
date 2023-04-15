71aca688bdc38c6512fce0a6c22a6b1f7e8a9b87
USER <kohadashish27@gmail.com>
2023-04-16 01:10:21 +0530

    hover

--- pages/collections/index.jsx
+++ pages/collections/index.jsx
@@ -3,15 +3,14 @@ import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
-  Heading,
   Image,
   SimpleGrid,
 } from "@chakra-ui/react";
-import Link from "next/link";
 import style from "./collections.module.css";
 import ProductFooter from "@/components/Product/ProductFooter";
 import Footer from "@/components/Footer/Footer";
 import { ChevronRightIcon } from "@chakra-ui/icons";
+import Link from "next/link";
 
 const collectionsArr = [
   {
@@ -30,27 +29,43 @@ const collectionsArr = [
     id: 3,
     title: "Chocolate Covered Teats",
     path: "/collections/Chocolate_Covered_Treats",
-    image: "",
+    image: "/Images/Products/Chocolate_Covered_Treats_banner.webp",
   },
   {
-    id: 1,
-    title: "BARS",
-    image: "",
+    id: 4,
+    title: "SMOOTH & CRUNCHY BARS",
+    image: "/Images/Products/Smooth_Crunchy_BARS_banner.webp",
+    path: "#",
   },
   {
-    id: 1,
-    title: "BARS",
-    image: "",
+    id: 5,
+    title: "CHOCOLATE SNACKS",
+    image: "/Images/Products/Snacks_banner.webp",
+    path: "Chocolate_Snacks",
   },
   {
-    id: 1,
-    title: "BARS",
-    image: "",
+    id: 6,
+    title: "GIFTS",
+    image: "/Images/Products/Gifts_banner.webp",
+    path: "Gifts",
   },
   {
-    id: 1,
-    title: "BARS",
-    image: "",
+    id: 7,
+    title: "CHOCOLATE COVERED TREATS",
+    image: "/Images/Products/Chocolate_Covered_Treats_banner.webp",
+    path: "#",
+  },
+  {
+    id: 8,
+    title: "DARK BARK",
+    image: "/Images/Products/Dark_Bark_banner.webp",
+    path: "#",
+  },
+  {
+    id: 8,
+    title: "BULK & BAKING CHOCOLATE",
+    image: "/Images/Products/Bulk_Baking_Chocolate_banner.webp",
+    path: "#",
   },
 ];
 
@@ -80,6 +95,7 @@ export default function Collections() {
       <Box bg="#2EBBCD" maxW="" h="157px" m="auto" className={style.BuyHeading}>
         <h1>BUY & SUBSCRIBE</h1>
       </Box>
+
       <SimpleGrid
         mt="10px"
         mb="10px"
@@ -96,7 +112,8 @@ export default function Collections() {
               // }}
             >
               <Image
-                src="https://cdn.shopify.com/s/files/1/0974/7668/t/16/assets/page_banner_6_image.jpg?v=107305128139824397671661403560"
+                // src="https://cdn.shopify.com/s/files/1/0974/7668/t/16/assets/page_banner_6_image.jpg?v=107305128139824397671661403560"
+                src={`${item.image}`}
                 alt=""
               />
               <div className={style.inside_container}>
