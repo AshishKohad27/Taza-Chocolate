d28eba45ddd6018e969507c397e827f38e02905b
AshishKohad27 <kohadashish27@gmail.com>
2024-01-01 22:27:15 +0530

    feat: add footer

--- src/components/client/footer.tsx
+++ src/components/client/footer.tsx
@@ -3,8 +3,98 @@ export default function Footer() {
     <div className="site-footer-outer">
       <div className="site-container">
         <div className="site-footer-inner">
-          <h3>Hello</h3>
-          <li>Hello Ashish</li>
+          {/* Upper Footer */}
+          <div className="upper-footer sf-grid">
+            {/* Left */}
+            <div className="left-footer sf-griditem">
+              <ul className="lf-list">
+                <h3>
+                  <a className="lf-list-a" href="#">
+                    bUY
+                  </a>
+                </h3>
+                <li className="lf-listitem">
+                  <a className="lf-listitem-a" href="">
+                    Hello
+                  </a>
+                </li>
+              </ul>
+              <ul className="lf-list">
+                <h3>
+                  <a className="lf-list-a" href="#">
+                    bUY
+                  </a>
+                </h3>
+                <li className="lf-listitem">
+                  <a className="lf-listitem-a" href="">
+                    Hello
+                  </a>
+                </li>
+              </ul>
+              <ul className="lf-list">
+                <h3>
+                  <a className="lf-list-a" href="#">
+                    bUY
+                  </a>
+                </h3>
+                <li className="lf-listitem">
+                  <a className="lf-listitem-a" href="">
+                    Hello
+                  </a>
+                </li>
+              </ul>
+              <ul className="lf-list">
+                <h3>
+                  <a className="lf-list-a" href="#">
+                    bUY
+                  </a>
+                </h3>
+                <li className="lf-listitem">
+                  <a className="lf-listitem-a" href="">
+                    Hello
+                  </a>
+                </li>
+              </ul>
+            </div>
+            {/* Right */}
+            <div className="right-footer sf-griditem">Right</div>
+          </div>
+          {/* Separator */}
+          <div className="sf-separator"></div>
+          {/* Lower Footer */}
+          <div className="lower-footer sf-grid">
+            {/* Left */}
+            <div className="left-footer sf-griditem">
+              <div className="footer-logo">
+                <img
+                  src="//www.tazachocolate.com/cdn/shop/t/20/assets/taza-white-footer.png?v=122262238396631087871701211120"
+                  alt="Taza Chocolate"
+                />
+              </div>
+              <div className="credits">
+                <h4>TAZA CHOCOLATE</h4>
+                <address>561 Windsor Street, Somerville MA 02143</address>
+                <p className="copyright">
+                  © 2024 Taza Chocolate. All rights reserved.
+                  <br />
+                  Web design by{" "}
+                  <a href="http://www.jumpingjackrabbit.com/" target="_blank">
+                    Jackrabbit
+                  </a>
+                </p>
+              </div>
+            </div>
+            {/* Right */}
+            <div className="right-footer sf-griditem">
+              <div className="badges">
+                <img
+                  className="badges-img"
+                  src="https://www.tazachocolate.com/cdn/shop/files/badge-bar-monochrome-2021_400x.png?v=1616597753"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
