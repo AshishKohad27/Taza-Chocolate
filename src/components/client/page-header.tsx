342782688e28a4bb9ef8749647e70799a261cf31
AshishKohad27 <kohadashish27@gmail.com>
2024-01-01 19:53:15 +0530

    feat: Category Grid Done

---
+++ src/components/client/page-header.tsx
@@ -0,0 +1,11 @@
+export default function PageHeader() {
+  return (
+    <div className="ph-outer">
+      <div className="site-container">
+        <div className="grid--center">
+          <h1>BUY & SUBSCRIBE</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
