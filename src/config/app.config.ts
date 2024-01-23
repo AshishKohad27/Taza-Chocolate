83953a85f129c22a267988d827aa0b40978a3272
AshishKohad27 <kohadashish27@gmail.com>
2024-01-23 21:12:45 +0530

    feat: new Folder structure Added

--- src/config/app.config.ts
+++
@@ -1,2 +0,0 @@
-export const REDUX_URL = 'http://localhost:3000/api'; // Development
-// export const REDUX_URL = 'http://localhost:3000/api'; // Production
--- src/pages/api/auth/forget-password.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import connectDB from "@/config/mongodb";
-import { AuthResponseControllerBET, AuthQueryBET } from "@/constant/server/auth-server";
-
-export default async function All_Auth(req: NextApiRequest, res: NextApiResponse) {
-    await connectDB;
-}
