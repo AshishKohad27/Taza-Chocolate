9f33c3ed3e096d3e55b3cb20140df20f63d4c416
AshishKohad27 <kohadashish27@gmail.com>
2024-01-28 12:31:49 +0530

    without api file

--- app/api/category/route.ts
+++
@@ -1,3 +0,0 @@
-export async function GET(request: Request) {
-    return Response.json({ 'message': 'Get Category' }, { status: 200 });
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-
-        {children}
+ 
+          <Header />
+          {children}
+     
       </body>
     </html>
   );
